ddac767348a9041e4620376032b937a1407d3721
anudeep5e7 <anudeeprachamalla@gmail.com>
2022-09-08 23:44:08 +0530

    methodM

---
+++ src/com/app/java/Test.java
@@ -0,0 +1,15 @@
+package com.app.java;
+
+public class Test {
+
+	public void m() {
+		System.out.println("java is open source");
+	}
+
+	public static void main(String[] args) {
+
+		Test t = new Test();
+
+		t.m();
+	}
+}
